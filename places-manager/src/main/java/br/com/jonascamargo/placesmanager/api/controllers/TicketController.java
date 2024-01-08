12ed31c762c9c0a66d75c5d9704a4e99a521a081
jonascamargoo <jonascamargo999@gmail.com>
2024-01-08 09:38:26 -0300

    feat: add Ticket controller and service

--- places-manager/src/main/java/br/com/jonascamargo/placesmanager/api/controllers/TicketController.java
+++ places-manager/src/main/java/br/com/jonascamargo/placesmanager/api/controllers/TicketController.java
@@ -1,5 +1,49 @@
 package br.com.jonascamargo.placesmanager.api.controllers;
 
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
+
+import java.util.List;
+import java.util.UUID;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.jonascamargo.placesmanager.infrastructure.dtos.TicketRecordDto;
+import br.com.jonascamargo.placesmanager.infrastructure.models.Ticket;
+import br.com.jonascamargo.placesmanager.logic.services.TicketService;
+import jakarta.validation.Valid;
+
+@RestController
 public class TicketController {
-    
+    private final TicketService ticketService;
+
+    public TicketController(TicketService ticketService) {
+        this.ticketService = ticketService;
+    }
+
+    @PostMapping("/passagens")
+    public ResponseEntity<Ticket> createTicket(@RequestBody @Valid TicketRecordDto ticketRecordDto) {
+        Ticket createdTicket = ticketService.createTicket(ticketRecordDto);
+        return ResponseEntity.status(HttpStatus.CREATED).body(createdTicket);
+    }
+
+    @GetMapping("/passagens")
+    public ResponseEntity<List<Ticket>> getTickets() {
+        List<Ticket> tickets = ticketService.getTickets();
+        return ResponseEntity.status(HttpStatus.OK).body(tickets);
+    }
+
+    @GetMapping("/passagens/{id}")
+    public ResponseEntity<Ticket> getTicketById(@PathVariable(value = "id") UUID id) {
+        Ticket ticket = ticketService.getTicketById(id);
+        ticket.add(linkTo(methodOn(TicketController.class).getTickets()).withRel("Lista de passagens"));
+        return ResponseEntity.status(HttpStatus.OK).body(ticket);
+    }
+
 }
--- places-manager/src/main/java/br/com/jonascamargo/placesmanager/logic/services/TicketService.java
+++ places-manager/src/main/java/br/com/jonascamargo/placesmanager/logic/services/TicketService.java
@@ -1,6 +1,8 @@
 package br.com.jonascamargo.placesmanager.logic.services;
 
 import java.time.Duration;
+import java.util.List;
+import java.util.UUID;
 
 import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Service;
@@ -9,6 +11,7 @@ import com.github.slugify.Slugify;
 
 import br.com.jonascamargo.placesmanager.infrastructure.dtos.TicketRecordDto;
 import br.com.jonascamargo.placesmanager.infrastructure.exceptions.customExceptions.InvalidTicketTimeException;
+import br.com.jonascamargo.placesmanager.infrastructure.exceptions.customExceptions.TicketNotFoundException;
 import br.com.jonascamargo.placesmanager.infrastructure.models.Ticket;
 import br.com.jonascamargo.placesmanager.infrastructure.repositories.TicketRepository;
 
@@ -31,6 +34,14 @@ public class TicketService {
         ticket.setSlug(slug.slugify(ticketRecordDto.destination()));
         return ticketRepository.save(ticket);
 
+    }
+
+    public List<Ticket> getTickets() {
+        return ticketRepository.findAll();
+    }
+
+    public Ticket getTicketById(UUID id) {
+        return ticketRepository.findById(id).orElseThrow(TicketNotFoundException::new);
     }
 
     // the purchase is only allowed 30 minutes prior to the departure time
