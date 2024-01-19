66a0c42a594ea7dff4cbcef3814437a72bfda3c4
jonascamargoo <jonascamargo999@gmail.com>
2024-01-19 12:23:16 -0300

    fix: fix place dto, removing date fields

--- places-manager/src/main/java/br/com/jonascamargo/placesmanager/infrastructure/dtos/PlaceRecordDto.java
+++ places-manager/src/main/java/br/com/jonascamargo/placesmanager/infrastructure/dtos/PlaceRecordDto.java
@@ -1,11 +1,9 @@
 package br.com.jonascamargo.placesmanager.infrastructure.dtos;
 
-import java.time.LocalDateTime;
 import jakarta.validation.constraints.NotBlank;
 
 public record PlaceRecordDto(
         @NotBlank String name,
-        String slug,
-        LocalDateTime createdAt, //preciso desse createdAt e updateAt aqui no DTO? talvez sim para poder utilizar do DTO para fazer as verificacoes e nao precisar instanciar para isso. Talvez nao, pois ambos nao sao fornecidos pelo client e sim gerados automaticamente
-        LocalDateTime updatedAt
+        String slug
+        
     ) {}
--- places-manager/src/test/java/br/com/jonascamargo/placesmanager/logic/services/PlaceServiceTest.java
+++ places-manager/src/test/java/br/com/jonascamargo/placesmanager/logic/services/PlaceServiceTest.java
@@ -34,7 +34,7 @@ public class PlaceServiceTest {
     @DisplayName("Should create a place successfully when everything is ok")
     void createPlace() {
         // Arrange
-        PlaceRecordDto placeRecordDto = new PlaceRecordDto("Test Place", null, null, null);
+        PlaceRecordDto placeRecordDto = new PlaceRecordDto("Test Place", null);
         // Mocking behavior
         when(placeRepository.save(any())).thenReturn(new Place());
         // Act
